e09f1f1c33088e27b7e72ee01775ae3ca56faa04
HailoRT-Automation <contact@hailo.ai>
2025-07-13 16:12:16 +0300

    v4.22.0

--- linux/vdma/memory.c
+++ linux/vdma/memory.c
@@ -167,9 +167,7 @@ struct hailo_vdma_buffer *hailo_vdma_buffer_map(struct device *dev,
     }
 
     if (HAILO_DMA_DMABUF_BUFFER != buffer_type) {
-        mmap_read_lock(current->mm);
         vma = find_vma(current->mm, addr_or_fd);
-        mmap_read_unlock(current->mm);
         if (IS_ENABLED(HAILO_SUPPORT_MMIO_DMA_MAPPING)) {
             if (NULL == vma) {
                 dev_err(dev, "no vma for virt_addr/size = 0x%08lx/0x%08zx\n", addr_or_fd, size);
